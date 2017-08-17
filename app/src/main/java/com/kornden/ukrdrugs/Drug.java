4ee707d7a627ba96ac50f18ba605efc8eeadce59
kornden <kornden@gmail.com>
2017-08-17 18:43:11 +0300

    field group is now predefined

--- app/src/main/java/com/kornden/ukrdrugs/Drug.java
+++ app/src/main/java/com/kornden/ukrdrugs/Drug.java
@@ -10,7 +10,7 @@ package com.kornden.ukrdrugs;
 public class Drug {
     private String name;
     private String dozes;
-    private String group;
+    private String group = "";
     private String latinName;
 
     /**
@@ -29,14 +29,13 @@ public class Drug {
 
     /**
      * temporary constructor?
-     * @param name must be international nonproprietary name of Drug in native language
+     * @param name must be international nonproprietary name of Drug in default language
      * @param latinName international nonproprietary name of Drug in Latin
      * @param dozes Dozes, which can be prescribed(30mg, 100mkg/d, 2.0ml - 3% etc).
      */
     public Drug(String name,String latinName, String dozes){
         this.latinName = latinName;
         this.name = name;
-        this.group = "";
         this.dozes = dozes;
     }
 
