499f62d1f887bbfa0969a86745a26be50af4c427
kornden <kornden@gmail.com>
2017-08-17 12:07:33 +0300

    add two more TextViews to be displayed

--- app/src/main/java/com/kornden/ukrdrugs/Drug.java
+++ app/src/main/java/com/kornden/ukrdrugs/Drug.java
@@ -1,27 +1,42 @@
 package com.kornden.ukrdrugs;
 
 /**
- * Created by kornd on 16-Aug-17.
+ * Created by kornden on 16-Aug-17.
+ * TODO: create this objects from excel files.
+ * TODO: add examples of recipes (need another class recipe?).
  */
+
 
 public class Drug {
     private String name;
     private String dozes;
     private String group;
+    private String latinName;
 
-    public Drug(String name, String group){
+    /**
+     *
+     * @param name must be international nonproprietary name of Drug in native language
+     * @param latinName international nonproprietary name of Drug in Latin
+     * @param group Types of medicines (For the digestive system, For the cardiovascular system etc)
+     * @param dozes Dozes, which can be prescribed(30mg, 100mkg/d, 2.0ml - 3% etc).
+     */
+    public Drug(String name,String latinName, String group, String dozes){
+        this.latinName = latinName;
         this.name = name;
         this.group = group;
-        dozes = "";
+        this.dozes = dozes;
     }
-    public Drug(String name){
+
+    /**
+     * temporary constructor?
+     * @param name must be international nonproprietary name of Drug in native language
+     * @param latinName international nonproprietary name of Drug in Latin
+     * @param dozes Dozes, which can be prescribed(30mg, 100mkg/d, 2.0ml - 3% etc).
+     */
+    public Drug(String name,String latinName, String dozes){
+        this.latinName = latinName;
         this.name = name;
         this.group = "";
-        this.dozes = "";
-    }
-    public Drug(String name, String group, String dozes){
-        this.name = name;
-        this.group = group;
         this.dozes = dozes;
     }
 
@@ -33,5 +48,8 @@ public class Drug {
     }
     public String getGroup(){
         return group;
+    }
+    public String getLatinName(){
+        return latinName;
     }
 }
